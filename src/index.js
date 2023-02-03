6898cd0135ab74424d0a49c666d8901769b91fab
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-02-03 19:50:19 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <><advertisement><center>Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me, you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@ybl .</center></advertisement><React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode><advertisement><center>Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me, you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@ybl .</center></advertisement></>
 );
 
 // If you want to start measuring performance in your app, pass a function
